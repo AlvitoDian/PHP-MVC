54f40dc4041bae37de60a75ea6fc1f743f9eb571
AlvitoDian <alvidpp23@gmail.com>
2023-07-03 13:35:59 +0700

    first commit

---
+++ app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1 class="mt3">About me</h1>
+    <img src="<?= BASEURL ?>/img/NOOT.jpg" alt="" width="300" class="rounded-circle shadow">
+    <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
